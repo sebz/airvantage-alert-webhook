fde3ac5e6d1dae1ed0acf69df9bd25a4e8fa65fc
Sebastien Moran <smoran@sierrawireless.com>
2016-08-04 15:14:34 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# airvantage-alert-webhook
+# :rotating_light: airvantage-alert-webhook
 Sample nodejs code to receive AirVantage alerts thru a webhook
+
+## Getting started
+> :bulb: Nodejs >= 4.x required
+
+1. Install dependencies `npm i`
+2. Start the server `npm start`
+3. Go to http://localhost:3000/ and prepare to watch the alerts comming
+4. Test by posting content on http://localhost:3000/alert
+     
+  ```bash
+  curl -H "Content-Type: application/json" -X POST -d '{"fake":"alert"}' http://localhost:3000/alert
+  ```
+
+
+## Real life usage
+
+1. Deploy this application on any public infrastructure
+2. Check out AirVantage user documentation https://doc.airvantage.net/av/reference/configure/ to setup the webhook
+3. And the API documenation https://doc.airvantage.net/av/reference/cloud/API/#API-Alert-Rule-v1/ (see _Configure Notification Hooks / Subscriber API_) for more details on what will be posted.
+
---
+++ server.js
@@ -0,0 +1,40 @@
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
+
+const app = express();
+const server = require('http').Server(app);
+const socketio = require('socket.io');
+const io = socketio(server);
+
+const socketClients = {};
+
+app.use(bodyParser.json());
+
+app.use(serveStatic('app'));
+
+app.engine('.html', require('ejs').__express);
+app.set('views', './views');
+app.set('view engine', 'html');
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/alert', (req, res) => {
+    console.log('/alert:', req.body);
+    _.each(socketClients, socket => socket.emit('alerts', req.body));
+    res.status(200).send('OK');
+});
+
+
+io.on('connection', socket => {
+    console.log('Connection of socket', socket.id);
+    socketClients[socket.id] = socket;
+    socket.emit('welcome', {});
+});
+
+app.set('port', process.env.PORT || 3000);
+server.listen(app.get("port"), () => console.log('AirVantage alert webhook app listening on port', app.get("port"), '\\o/'));
